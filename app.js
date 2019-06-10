713ae0a1364f3a13a1526b170dbd48eb9729db4e
Yurii Rudenko <yurii.rudenko@masterofcode.com>
2019-06-10 09:20:00 +0300

    refactoring

--- app.js
+++ app.js
@@ -5,23 +5,17 @@ let url = `${constants.url}${constants.apiToken}/sendMessage`;
 app.use(constants.bodyParser.json());
 
 app.post('/', (req, res) => {
-    if (req.body.message) {
-        const chatId = req.body.message.chat.id;
-        const sentMessage = req.body.message.text;
+    if (!req.body.message) return;
 
-        if (!sentMessage.match(/hello/)) return res.status(200).send({});
-        constants.axios
-            .post(url, {
-                chat_id: chatId,
-                text: 'hello back 👋'
-            })
-            .then((response) => {
-                res.status(200).send(response);
-            })
-            .catch((error) => {
-                res.send(error);
-            });
-    }
+    const chatId      = req.body.message.chat.id;
+    const sentMessage = req.body.message.text;
+    let   params      = { chat_id: chatId, text: 'hello back 👋' };
+
+    if (!sentMessage.match(/hello/)) return res.status(200).send({});
+    constants.axios
+        .post(url, params)
+        .then((res) => res.status(200).send(res))
+        .catch((err) => res.send(err));
 });
 
 app.listen(constants.port, () => {

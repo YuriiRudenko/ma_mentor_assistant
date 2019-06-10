518c8a04c873e30296984d54405f9239505ef478
Yurii Rudenko <yurii.rudenko@masterofcode.com>
2019-06-10 09:22:01 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let constants = require('./constants');
-let app = constants.app;
-let url = `${constants.url}${constants.apiToken}/sendMessage`;
+let app       = constants.app;
+let url       = `${constants.url}${constants.apiToken}/sendMessage`;
 
 app.use(constants.bodyParser.json());
 
@@ -9,9 +9,10 @@ app.post('/', (req, res) => {
 
     const chatId      = req.body.message.chat.id;
     const sentMessage = req.body.message.text;
-    let   params      = { chat_id: chatId, text: 'hello back 👋' };
+    let   params      = { chat_id: chatId, text: 'hello back' };
 
-    if (!sentMessage.match(/hello/)) return res.status(200).send({});
+    if (!sentMessage.match(/hello/)) return res.status(200).send(null);
+
     constants.axios
         .post(url, params)
         .then((res) => res.status(200).send(res))

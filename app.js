305b183df54aecb07544d5e653c9db65188e28a5
Yurii Rudenko <yurii.rudenko@masterofcode.com>
2019-06-25 20:56:48 +0300

    update source

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 let constants = require('./constants');
-//
 let app       = constants.app;
 let url       = `${constants.url}${constants.apiToken}/sendMessage`;
-let db = require('./db');
+let db        = require('./db');
+// let google = require('./google');
+
 
 const Source = db.source(db.connector, db.adapter);
 
 app.use(constants.bodyParser.json());
+
+
+// sheet_id = '1g_fmYBreEfglD_Qi2p0ue5JQYpWGbRUP2TIOX-HKVwc';
+// processor = (err, res) => {
+//     if (err) return console.log('The API returned an error: ' + err);
+//     const rows = res.data.values;
+//     if (rows.length) {
+//         console.log('Name, Major:');
+//         // Print columns A and E, which correspond to indices 0 and 4.
+//         rows.map((row) => {
+//
+//             console.log(`${row}`);
+//         });
+//     } else {
+//         console.log('No data found.');
+//     }
+// };
+//
+// google.readSheet();
+
+findOrCreateSourceByKind = (kind, token) => {
+    Source.findOne({where: { kind: kind }}).then((record) => {
+        if (!record) record = Source.build({ kind: kind });
+        record.token = token;
+        record.save();
+    });
+    return kind;
+};
 
 app.post('/', (req, res) => {
     if (!req.body.message) return;
 
     const chatId      = req.body.message.chat.id;
     const sentMessage = req.body.message.text;
-    let   params      = { chat_id: chatId, text: 'hello back' };
+    let   params      = { chat_id: chatId };
 
-    if (sentMessage.match(/\\source/)){
-        console.log(123);
-        Source.findAll().then((sources => {
-                if (sources.length) console.log(sources[0].dataValues.token);
-            })
-        );
+    if (sentMessage.match(/\/source.+/)){
+        let data = sentMessage.split(' ');
+        let kind = findOrCreateSourceByKind(data[1], data[2]);
+
+        params['text'] = `Successfully saved source for ${kind}`;
+
+        constants.axios
+            .post(url, params)
+            .then((res) => res.status(200).send(res))
+            .catch((err) => res.send(err));
     }
     else if (sentMessage.match(/hello/)){
         constants.axios
